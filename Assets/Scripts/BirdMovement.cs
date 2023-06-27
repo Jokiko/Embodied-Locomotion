dd10a4e9fc7aadbd42cb9311930160176179a990
Jokiko <75935156+Jokiko@users.noreply.github.com>
2023-06-27 17:34:31 +0200

    Parcour Start & Adjustments
    -added assets for flight parcour
    -changed first person camera to be slightly ahead of bird
    -added collision detection to bird
    -added collision counter text to scene

--- Assets/Scripts/BirdMovement.cs
+++ Assets/Scripts/BirdMovement.cs
@@ -2,13 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using OpenCVForUnity.CoreModule;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BirdMovement : MonoBehaviour
 {
     private FaceTracker faceTracker;
 
+    private GameObject collisionCounter;
+    private Text textComponent;
+    private float collisions = 0f;
+
     public float initialMovementSpeed = 1f;
     private float movementSpeed;
+
+    private float maxHeight = 10f;
+
+    private Rigidbody rb;
 
     OpenCVForUnity.CoreModule.Rect lastLegitRect;
 
@@ -25,7 +34,11 @@ public class BirdMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-         faceTracker = FindObjectOfType<FaceTracker>();
+        faceTracker = FindObjectOfType<FaceTracker>();
+        collisionCounter = GameObject.Find("CollisionCounter");
+        textComponent = collisionCounter.GetComponent<Text>();
+        textComponent.text = "Anzahl Kollisionen: 0";
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -54,20 +67,42 @@ public class BirdMovement : MonoBehaviour
 
                 movementSpeed = initialMovementSpeed * (latestRect.width/250f) * (latestRect.width/250f) ;
 
-                Debug.Log("MovementSpeed: "+movementSpeed);
+                //Debug.Log("MovementSpeed: "+movementSpeed);
 
 
                 // Basierend auf getrackten Koordinaten des Gesichts Werte mappen
                 float mappedX = Mathf.Lerp(xMapMin, xMapMax, Mathf.InverseLerp(xRangeMin, xRangeMax, targetX)) * -1;
                 //Debug.Log(mappedX);  
                 float mappedY = Mathf.Lerp(yMapMin, yMapMax, Mathf.InverseLerp(yRangeMin, yRangeMax, targetY)) * -1;             
-
+                
                 Vector3 movement = new Vector3(mappedX, mappedY, initialMovementSpeed); 
                 movement = movement.normalized * movementSpeed * Time.deltaTime;
+
+                //verhindern,d ass der Vogel in den Boden fliegt
+                if (transform.position.y + movement.y < 0f)
+                {
+                    movement.y = Mathf.Max(0f - transform.position.y, 0f); 
+                }
+                //verhindern, dass der Vogel ueber die maximalhohe fliegt
+                else if (transform.position.y + movement.y > maxHeight)
+                {
+                    movement.y = Mathf.Min(maxHeight - transform.position.y, 0f); 
+                }
 
                 transform.Translate(movement);
             }
 
         }
     }
+
+    void OnTriggerEnter(Collider collision)
+{
+    //Physics.IgnoreCollision(collision.collider, GetComponent<Collider>(), true);
+    //Update Schriftzug bei Kollision mit Hindernis
+    if (collision.gameObject.CompareTag("Baum"))
+    {   
+        collisions++;
+        textComponent.text = "Anzahl Kollisionen: "+collisions;
+    } 
+}
 }
--- readme.txt
+++ readme.txt
@@ -7,3 +7,7 @@
 Tiefendimension zur Kontrolle von Fluggeschwindigkeit
 
 Idee für Parcour: langer Korridor, wenig Kollisionen und kein Drehen/Wechsel von Flugrichtung nötig (siehe Problematik Rotation)
+
+Externals:
+-Darth Artisan's Free Trees: siehe Übung 2
+-Customizable Skybox https://assetstore.unity.com/packages/2d/textures-materials/sky/customizable-skybox-174576
